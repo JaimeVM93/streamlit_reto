c575ced7e2dfe8ddcd42230484554d9026c008ab
JaimeVM93 <jaime.vasquez@sgbsal.com>
2024-11-20 08:18:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds)
 
-##@st.cache
+@st.cache
 def load_data():
    dbMovies = list(db.collection('movies').stream())
    movies_dict = list(map(lambda x: x.to_dict(), dbMovies))
